150753cf1eb90ae6032cbf1b77f70c95fbc530fc
최서연 <yeondublue@naver.com>
2022-08-23 20:25:35 +0900

    first commit

---
+++ SwiftUI_iTunes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_iTunes
+//
+//  Created by yeonBlue on 2022/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_iTunes/SwiftUI_iTunesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_iTunesApp.swift
+//  SwiftUI_iTunes
+//
+//  Created by yeonBlue on 2022/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_iTunesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
